2293f489d49e5a70c7c74760353425805ce74fd3
x247978 <X247978@one.merckgroup.com>
2022-08-21 14:28:02 +0530

    updated

---
+++ FormsLoyalty/FormsLoyalty.iOS/Services/AppSettingsServices.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using FormsLoyalty.Interfaces;
+using FormsLoyalty.iOS.Services;
+using Foundation;
+using WebKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AppSettingsServices))]
+
+namespace FormsLoyalty.iOS.Services
+{
+    public class AppSettingsServices : IAppSettings
+    {
+        
+        public bool CheckIfAppInstalledOrNot(string packageName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearAllCookies()
+        {
+            //NSHttpCookieStorage CookieStorage = NSHttpCookieStorage.SharedStorage;
+            //foreach (var cookie in CookieStorage.Cookies)
+            //{
+            //    CookieStorage.DeleteCookie(cookie);
+            //}
+
+            NSHttpCookieStorage.SharedStorage.RemoveCookiesSinceDate(NSDate.DistantPast);
+
+            WKWebsiteDataStore.DefaultDataStore.FetchDataRecordsOfTypes(WKWebsiteDataStore.AllWebsiteDataTypes, (NSArray records) =>
+            {
+
+                for (System.nuint i = 0; i < records.Count; i++)
+                {
+                    var record = records.GetItem<WKWebsiteDataRecord>(i);
+                    WKWebsiteDataRecord[] recordArray = new WKWebsiteDataRecord[record.DataTypes.Count];
+                    WKWebsiteDataStore.DefaultDataStore.RemoveDataOfTypes(record.DataTypes, NSDate.DistantPast, () => { });
+                }
+
+            });
+        }
+
+        public string GetOSVersion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SwitchToBackground()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ FormsLoyalty/FormsLoyalty/Behaviors/DecimalNumberValidationBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FormsLoyalty.Behaviors
+{
+    public class DecimalNumberValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += Entry_TextChanged;
+            base.OnAttachedTo(entry);
+        }
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= Entry_TextChanged;
+            base.OnDetachingFrom(entry);
+        }
+        private void Entry_TextChanged(object sender, TextChangedEventArgs args)
+        {
+            if (!string.IsNullOrWhiteSpace(args.NewTextValue))
+            {
+                bool isvalid = args.NewTextValue.ToCharArray().All(x => char.IsDigit(x));
+                ((Entry)sender).Text = isvalid ? args.NewTextValue : args.NewTextValue.Remove(args.NewTextValue.Length - 1);
+            }
+        }
+    }
+}
